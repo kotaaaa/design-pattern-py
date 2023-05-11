6eeb5affa2796d887240cc184c605bfb8a2a228a
kotaaaa <kota.k.1132.pda@gmail.com>
2023-05-11 19:00:07 +0900

    add visitor and comment

---
+++ visitor/visitor.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+
+# There is no Overload in Python. so skip for now
+# class Visitor(metaclass=ABCMeta):
+#     @abstractmethod
+#     def visit(self, file):
+#         pass
+#     @abstractmethod
+#     def visit(self, directory):
+#         pass
