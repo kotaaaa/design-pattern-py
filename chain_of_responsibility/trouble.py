647ae970c283293d17bb8953efcf95b5b20f5e0a
kotaaaa <kota.k.1132.pda@gmail.com>
2023-05-12 19:18:26 +0900

    add trouble.py

---
+++ chain_of_responsibility/trouble.py
@@ -0,0 +1,10 @@
+class Trouble():
+    def __init__(self, number):
+        self.number = number
+
+    def get_number(self):
+        return self.number
+
+    def to_string(self):
+        return "[Trouble ]" + number + "]"
+
