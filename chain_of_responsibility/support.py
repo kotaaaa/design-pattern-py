b56551694036912bb00bb1f6c17300541f0cda16
kotaaaa <kota.k.1132.pda@gmail.com>
2023-05-13 12:13:20 +0900

    add support

---
+++ chain_of_responsibility/support.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Support(metaclass=ABCMeta):
+    def __init__(self, name):
+        self.name = name
+    
+    def set_next(self, next):
+        self.next = next
+        return self.next
+    
+    def support(trouble):
+        if self.resolve(trouble):
+            done(trouble)
+        elif self.next is not None:
+            self.next.support(trouble)
+        else:
+            self.fail(trouble)
+
+    def to_string(self):
+        return "[" + self.name +"]"
+
+    @abstractmethod
+    def resolve(self):
+        pass
+
+    def done(self, trouble):
+        print(self.trouble + " is resolved by " + this + ".")
+
+    def fail(self, trouble):
+        print(self.trouble + "cannot be resolved.")
