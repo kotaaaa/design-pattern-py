8ad862936498eccf3c2d7e9870e686b4d6b30cbc
kotaaaa <kota.k.1132.pda@gmail.com>
2023-05-16 17:58:53 +0900

    add limit_support.py

---
+++ chain_of_responsibility/limit_support.py
@@ -0,0 +1,12 @@
+from support import Support
+
+class LimitSupport(Support):
+    def __init__(self, name, limit):
+        super().__init__(caption)
+        self.limit = limit
+
+    def resolve(self, trouble):
+        if trouble.get_number() < limit:
+            return True
+        else:
+            return False
